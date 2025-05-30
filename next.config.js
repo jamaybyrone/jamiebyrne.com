7a69404d3f4406328f31d2d760b2a4d65ab304dd
jamay byrone <jamie@jamiebyrne.com>
2025-05-30 07:42:35 +0100

    labels and csp

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const {NODE_ENV} = process.env
 const cspHeader = `
     default-src 'self' *.google-analytics.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
-    frame-src 'self' *.paypal.com;
+    frame-src 'self';
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
--- src/components/layout/navigation/social/index.tsx
+++ src/components/layout/navigation/social/index.tsx
@@ -9,13 +9,28 @@ import React from 'react'
 export const Social = () => {
   return (
     <Paper elevation={0} sx={{ textAlign: 'center', mt: 2 }}>
-      <Link href={GIT_HUB_URL} target={'_blank'} style={{ margin: '5px' }}>
+      <Link
+        href={GIT_HUB_URL}
+        target={'_blank'}
+        style={{ margin: '5px' }}
+        aria-label="GitHub"
+      >
         <GitHubIcon />
       </Link>
-      <Link href={LINKEDIN_URL} target={'_blank'} style={{ margin: '5px' }}>
+      <Link
+        href={LINKEDIN_URL}
+        target={'_blank'}
+        style={{ margin: '5px' }}
+        aria-label="LinkedIn"
+      >
         <LinkedInIcon />
       </Link>
-      <Link href={'/api/cv'} target={'_blank'} style={{ margin: '5px' }}>
+      <Link
+        href={'/api/cv'}
+        target={'_blank'}
+        style={{ margin: '5px' }}
+        aria-label="CV Download"
+      >
         <ArticleIcon />
       </Link>
     </Paper>
